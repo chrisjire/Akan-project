1ac49214410cf180369ad4b15df375a889f8c3aa
chris <christianwilliamsjire@gmail.com>
2019-07-14 14:02:27 +0300

    add if/else for male and female

--- javascript/script.js
+++ javascript/script.js
@@ -8,4 +8,36 @@ var dd = parseInt(prompt("Enter the date of birth"));
 var gender= prompt("your gender")
 var result=day(cc,yy,mm,dd).toFixed(); 
 var males = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];  
+var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; 
+if (gender == "male" && result == 0) {
+    alert(males[0]);
+  } else if (gender == "male" && result == 1) {
+    alert(males[1]);
+  } else if (gender == "male" && result == 2) {
+    alert(males[2]);
+  } else if (gender == "male" && result == 3) {
+    alert(males[3]);
+  } else if (gender == "male" && result == 4) {
+    alert(males[4]);
+  } else if (gender == "male" && result == 5) {
+    alert(males[5]);
+  } else if (gender == "male" && result == 6) {
+    alert(males[6]);
+  } else if (gender == "female" && result == 0) {
+    alert(females[0]);
+  } else if (gender == "female" && result == 1) {
+    alert(females[1]);
+  } else if (gender == "female" && result == 2) {
+    alert(females[2]);
+  } else if (gender == "female" && result == 3) {
+    alert(females[3]);
+  } else if (gender == "female" && result == 4) {
+    alert(females[4]);
+  } else if (gender == "female" && result == 5) {
+    alert(females[5]);
+  } else if (gender == "female" && result == 6) {
+    alert(females[6]);
+  } else {
+    alert("null");
+  }
+  
