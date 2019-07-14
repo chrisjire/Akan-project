03a58aec55b7195a4c97827b334a9637fc3a63e8
chris <christianwilliamsjire@gmail.com>
2019-07-14 13:57:46 +0300

    add variables for century, year, month, date of birth, gender and results

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,9 @@
 function day (cc,yy,mm,dd){
     return ((((cc/19)+1*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
     }
+var cc = parseInt(prompt("Enter the century")); 
+var yy = parseInt(prompt("Enter the year"));
+var mm = parseInt(prompt("Enter the month"));
+var dd = parseInt(prompt("Enter the date of birth"));
+var gender= prompt("your gender")
+var result=day(cc,yy,mm,dd).toFixed();   
